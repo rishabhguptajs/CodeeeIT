f9b34045c7f3a929db5010cc09951ec9ae4d0949
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-07-28 01:58:14 +0530

    chore: update npm dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['assets.aceternity.com'], // Add the domain here
+      },
+};
 
 export default nextConfig;
